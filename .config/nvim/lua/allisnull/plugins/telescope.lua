03d598961444f2a8f697c43b554dc6fabf0a66cc
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-07-06 17:46:05 -0400

    Update telescope.nvim

--- .config/nvim/lua/allisnull/plugins/telescope.lua
+++ .config/nvim/lua/allisnull/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -78,8 +77,6 @@ return {
         vim.keymap.set("n", "<leader>f", "", { desc = "Find" })
         vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
         vim.keymap.set("n", "<leader>fs", telescope.extensions.live_grep_args.live_grep_args, { desc = "Fuzzy find string in cwd" })
-        vim.keymap.set("v", "<leader>f", lga_shortcuts.grep_visual_selection, { desc = "Fuzzy find selection in cwd" })
-        vim.keymap.set("n", "<leader>fs", telescope.extensions.live_grep_args.live_grep_args, { desc = "Fuzzy find string in cwd" })
         vim.keymap.set("n", "<leader>fw", lga_shortcuts.grep_word_under_cursor, { desc = "Fuzzy find word under cursor in cwd" })
         vim.keymap.set("n", "<leader>fW", function()
             telescope.extensions.live_grep_args.live_grep_args({
@@ -97,5 +94,6 @@ return {
         vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "Fuzzy find todos" })
         vim.keymap.set("n", "<leader>fd", ":Telescope diagnostics<CR>", { desc = "Fuzzy find diagnostics" })
         vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Fuzzy find keymaps" })
+        vim.keymap.set("v", "<leader>f", lga_shortcuts.grep_visual_selection, { desc = "Fuzzy find selection in cwd" })
     end,
 }
